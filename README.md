44bf6fbab9bb43a292a0e8cc3926bfcd57b266c6
dragana_kor <dragana.korunovic@gmail.com>
2025-02-17 19:38:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# demo-sso
+An SSO server based on Spring Authorization server and a resource server.
+
+This project is an exercise and a practical illustration to the articles 2 and 3 of **Building SSO based on Spring Authorization Server[https://medium.com/@d.snezhinskiy/building-sso-based-on-spring-authorization-server-part-2-of-3-fc0e5db83609]** by D. Snezhinskiy. It consists of an authorization and a resource server.
+
+The authorization server implements code grant authorization and grant password authorization, using opaque tokens. Users, roles and authorities are stored in a PostgreSQL database. Authorization using Social Login (Google) is also provided.
+
+The resource server is configured to retrieve a list of user authorities from the authorization server, using an opaque token. There are demo endpoints defined in the resource server that are accessible only to users with certain authorities.
---
+++ authorizationServer/src/main/java/com/demosso/authorizationserver/configuration/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.demosso.authorizationserver.configuration;
+
+import com.demosso.authorizationserver.security.socialLogin.SocialLoginAuthenticationSuccessHandler;
+import com.demosso.authorizationserver.security.socialLogin.UserServiceOAuth2UserHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http,
+        AuthenticationSuccessHandler authenticationSuccessHandler
+    ) throws Exception {
+        return http
+            .authorizeHttpRequests(
+                authorize -> authorize
+                    .anyRequest().authenticated()
+            )
+            .formLogin(withDefaults())
+            .oauth2Login(oauth ->
+                oauth
+                    .successHandler(authenticationSuccessHandler)
+            )
+            .logout((logout) -> logout.permitAll()) //add all logout URIs to the permit list
+            .build();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler(UserServiceOAuth2UserHandler handler) {
+        SocialLoginAuthenticationSuccessHandler authenticationSuccessHandler =
+            new SocialLoginAuthenticationSuccessHandler();
+        authenticationSuccessHandler.setOidcUserHandler(handler);
+        return authenticationSuccessHandler;
+    }
+}
